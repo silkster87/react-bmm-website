b43b7c44da8ec22c9cf067ca256c5f491b51a843
Silky Ng <silky.ng@gmail.com>
2023-06-25 20:04:04 +0100

    website links

---
+++ src/links/website-links.tsx
@@ -0,0 +1,5 @@
+export const links = {
+  googleMapSrc: `https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&q=Christ+Church%2C+Tudor+Way%2C+Petts+Wood%2C+Kent%2C+BR5+1LH&zoom=15`,
+  joinBMM: 'https://www.cognitoforms.com/BromleyMusicMakers/BromleyMusicMakersApplicationForm',
+  exampleProgramme: 'https://drive.google.com/file/d/1HPbwlRXAT3aqaUBBhO9PMXiFoFEjPNUM/view?usp=sharing'
+}
