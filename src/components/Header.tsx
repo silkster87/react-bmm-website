b0405277f61fe1abd36e680881e6f084dd2f07af
Silky Ng <silky.ng@gmail.com>
2023-06-25 10:09:10 +0100

    header with menu and dropdown links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,69 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import bmmLogo from '../assets/bmm_logo.png'
 
+import MenuIcon from '@mui/icons-material/Menu'
+import CloseIcon from '@mui/icons-material/Close'
 import '../styles/Header.scss'
 
 export const Header = () => {
+  const [showMenu, setShowMenu] = useState(true)
+  const [showDropDown, setShowDropDown] = useState(false)
+
+  const closeDropDown = () => {
+    setShowDropDown(false)
+  }
+  const links = () => (
+    <>
+      <a href="#concerts" onClick={closeDropDown}>Concerts</a>
+      <a href="#about" onClick={closeDropDown}>About</a>
+      <a href="#contact" onClick={closeDropDown}>Contact</a>
+    </>
+  )
+
+  const iconSizes = { width: 32, height: 32 }
+  useEffect(() => {
+    const handleMenu = () => {
+      if (window.innerWidth > 980) {
+        setShowMenu(false)
+        setShowDropDown(false)
+      }
+      if (window.innerWidth <= 980) setShowMenu(true)
+    }
+
+    window.addEventListener('resize', handleMenu)
+    handleMenu()
+
+    return () => window.removeEventListener('resize', handleMenu)
+  }, [])
+
   return (
     <header>
       <nav>
-        <img src={bmmLogo} height="36px" width="36px"
-          alt="Bromley Music Makers Logo" />
-        <p>Bromley Music Makers</p>
-        <aside>
-          <a href="#concerts">Concerts</a>
-          <a href="#about">About</a>
-          <a href="#contact">Contact</a>
+        <div className="logo-title-container">
+          <img src={bmmLogo} height="36px" width="36px"
+            alt="Bromley Music Makers Logo" />
+          <p>Bromley Music Makers</p>
+        </div>
+        <aside className="links">
+          {links()}
         </aside>
+        {showMenu && !showDropDown &&
+          <button className="menu-btn" onClick={() => { setShowDropDown(prev => !prev) }}>
+            <MenuIcon sx={iconSizes} />
+          </button>}
+        {showDropDown &&
+          <button className="menu-btn" onClick={() => { setShowDropDown(prev => !prev) }}>
+            <CloseIcon sx={iconSizes} />
+          </button>
+        }
       </nav>
+      {showDropDown &&
+        <div className="dropdown-list-links">
+          <div className="links-dropdown-container">
+            {links()}
+          </div>
+        </div>}
     </header>
+
   )
 }
