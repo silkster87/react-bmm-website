fe56562e5f658093b4db8f164a66d444f795ee50
Silky Ng <silky.ng@gmail.com>
2023-06-26 20:45:27 +0100

    About section with styles

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import angela from '../assets/angela_carey.jpg'
+import martin from '../assets/martin_sharpe.jpg'
+import lois from '../assets/lois_jones.jpeg'
 import { links } from '../links/website-links'
 import '../styles/Main.scss'
 
@@ -19,9 +22,11 @@ export const Main = () => {
       </div>
       <p>General subscription will be <strong>£25</strong> for the season of concerts. The subscription is waived for full-time students at music college. For former students and all other members aged 18-29 the subscription is £5. </p>
       <p>Guests are welcome to attend concerts at a charge of <strong>£5</strong>.</p>
-      <button className="join-bmm-btn">
-        <a href={links.joinBMM} target="_blank" style={{ color: 'white' }} rel="noreferrer">Join BMM</a>
-      </button>
+      <div className="btn-container">
+        <button className="join-bmm-btn">
+          <a href={links.joinBMM} target="_blank" style={{ color: 'white' }} rel="noreferrer">Join BMM</a>
+        </button>
+      </div>
       <p>Members have the opportunity to hear or take part in performances of classical music in the form of solo items or small ensembles. Music lovers of all ages are welcome, including:</p>
       <ul>
         <li>Listening members</li>
@@ -52,15 +57,35 @@ export const Main = () => {
         Saturday June 22nd<br />
         </p>
         <p>An example programme is available to download here: </p>
-        <button className="join-bmm-btn">
-          <a href={links.exampleProgramme} target="_blank" rel="noreferrer">Download</a>
-        </button>
+        <div className="btn-container">
+          <button className="join-bmm-btn">
+            <a href={links.exampleProgramme} target="_blank" rel="noreferrer">Download</a>
+          </button>
+        </div>
       </div>
       <div className="about-container" id="about">
         <h2>About BMM</h2>
         <p>{'The membership of Bromley Music Makers consists of both listeners and performers. We are a large group with diverse musical interests, naturally all sharing a love of classical music.'}</p>
         <p>{'Pianists who join us have the opportunity to perform as soloists, duettists or accompanists. Other stringed or wind instrumentalists may perform as soloists or as part of a trio, quartet or larger chamber ensemble. If you are a singer or instrumentalist and you need an accompanist, we’ll help you to find one from amongst the membership.'}</p>
         <p>{'One of the benefits of joining Bromley Music Makers is finding other like-minded musicians to play and perform with. Many musical partnerships and groups have been formed over the years in this way.'}</p>
+      </div>
+      <div className="contact-container" id="contact">
+        <h2>Contact</h2>
+      </div>
+      <p>{'If you would like to know more about Bromley Music Makers please contact the secretary by email, or you may also contact the chairman by telephone. Alternatively, simply come along to our next concert. Guests are always welcome and you will have the opportunity to speak to the Secretary or one of the other committee members before the concert and during the interval.'}</p>
+      <div className="profiles-container">
+        <div className="profile-row">
+          <img alt="Martin Sharpe" src={martin} width="182px" height="251" />
+          <p>Chairman, Martin Sharpe - Tel: <a href={links.telMartinSharpe}>{'(020) 8402 1947'}</a></p>
+        </div>
+        <div className="profile-row">
+          <img alt="Angela Carey" src={angela} width="182px" height="251"/>
+          <p>Secretary, Angela Carey - Email: <a href={links.emailBMM}>bromleymusicmakers@gmail.com</a></p>
+        </div>
+        <div className="profile-row">
+          <img alt="Lois Jones" src={lois} width="182px" height="251"/>
+          <p>Music Secretary, Lois Jones - Email: <a href={links.emailMusicSec}>loisjones958@gmail.com</a></p>
+        </div>
       </div>
     </main>
   )
--- src/links/website-links.tsx
+++ src/links/website-links.tsx
@@ -1,5 +1,8 @@
 export const links = {
   googleMapSrc: `https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&q=Christ+Church%2C+Tudor+Way%2C+Petts+Wood%2C+Kent%2C+BR5+1LH&zoom=15`,
   joinBMM: 'https://www.cognitoforms.com/BromleyMusicMakers/BromleyMusicMakersApplicationForm',
-  exampleProgramme: 'https://drive.google.com/file/d/1HPbwlRXAT3aqaUBBhO9PMXiFoFEjPNUM/view?usp=sharing'
+  exampleProgramme: 'https://drive.google.com/file/d/1HPbwlRXAT3aqaUBBhO9PMXiFoFEjPNUM/view?usp=sharing',
+  telMartinSharpe: 'tel:+4402084021947',
+  emailBMM: 'mailto:bromleymusicmakers@gmail.com',
+  emailMusicSec: 'mailto:loisjones958@gmail.com'
 }
