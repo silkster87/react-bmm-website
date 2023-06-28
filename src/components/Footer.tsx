d5c450e7f61a7c13b26422cf7a763ccb89c66e5f
Silky Ng <silky.ng@gmail.com>
2023-06-28 19:54:06 +0100

    edited footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
+import '../styles/Footer.scss'
 import { links } from '../links/website-links'
 
 export const Footer = () => {
   return (
     <footer>
-      <p>Website designed and built by Silky Ng <a href={links.emailSilky}>silky.ng@gmail.com</a></p>
+      <p>Website designed and built by Silky Ng | Email: <a href={links.emailSilky}>silky.ng@gmail.com</a></p>
     </footer>
   )
 }
