8352e1b26a45cbbed1c0cea2d95493c269e45fa9
Silky Ng <silky.ng@gmail.com>
2023-06-28 19:38:33 +0100

    added logo to the banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
+import logo from '../assets/bmm_logo.png'
 import '../styles/Banner.scss'
 
 export const Banner = () => {
   return (
     <div id="banner">
+      <div className="bmm-logo-container">
+        <img className="bmm-logo" src={logo} alt="Bromley Music Makers Logo" height="90px" width="auto"/>
+      </div>
       <div className="banner-title">
         <p>Bromley Music Makers</p>
         <p className="smallprint">79th Season</p>
