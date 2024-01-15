24935dab9ccebcf38b989dc59b8531193743f45c
silkster87 <silky.ng@gmail.com>
2024-01-15 20:50:05 +0000

    Updated programme

--- src/links/website-links.tsx
+++ src/links/website-links.tsx
@@ -1,7 +1,7 @@
 export const links = Object.freeze({
   googleMapSrc: `https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&q=Christ+Church%2C+Tudor+Way%2C+Petts+Wood%2C+Kent%2C+BR5+1LH&zoom=15`,
   joinBMM: 'https://www.cognitoforms.com/BromleyMusicMakers/BromleyMusicMakersApplicationForm',
-  exampleProgramme: 'https://drive.google.com/file/d/1HPbwlRXAT3aqaUBBhO9PMXiFoFEjPNUM/view?usp=sharing',
+  exampleProgramme: 'https://drive.google.com/file/d/1jynyHaorcY7W1J8xFp-SHSQxjmP51A1Q/view?usp=sharing',
   telMartinSharpe: 'tel:+4402084021947',
   emailBMM: 'mailto:bromleymusicmakers@gmail.com',
   emailMusicSec: 'mailto:loisjones958@gmail.com',
