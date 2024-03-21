08c0540f0cda41678f731c4729d00c6761fec712
silkster87 <silky.ng@gmail.com>
2024-03-21 10:20:32 +0000

    update: pictures for Carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -6,6 +6,8 @@ import yoheiMartin from '../assets/yohei_martin.jpg'
 import yoheiMartinWebp from '../assets/yohei_martin.webp'
 import trioPic1 from '../assets/IMG_1566_resized.webp'
 import quintetPic1 from '../assets/IMG_1573_resized.webp'
+import trioPic2 from '../assets/IMG_2332.webp'
+import flutePlayer from '../assets/IMG_2336.webp'
 
 import '../styles/Carousel.scss'
 
@@ -24,6 +26,16 @@ export const ImageCarousel = () => {
   return (
     <div className="image-carousel">
       <Slider { ...settings }>
+        <div>
+          <picture>
+            <img src={trioPic2} width="800" height="600" alt="trio of musicians"/>
+          </picture>
+        </div>
+        <div>
+          <picture>
+            <img src={flutePlayer} width="800" height="600" alt="flute player"/>
+          </picture>
+        </div>
         <div>
           <picture>
             <img src={trioPic1} width="800" height="600" alt="trio of musicians"/>
